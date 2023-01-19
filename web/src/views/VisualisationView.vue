dfbf0f9587bd4997261be4e2ce89febf558748b7
tibvdm <tibovandemoortele@hotmail.com>
2023-01-19 17:27:42 +0100

    add back button

--- web/src/views/VisualisationView.vue
+++ web/src/views/VisualisationView.vue
@@ -5,7 +5,7 @@
             <span class="text-h6">Legend</span>
             <div v-for="taxon of visualisationStore.highlightedTaxa" :key="taxon" class="d-flex">
                 <div :style="{ width: '20px', height: '20px', background: computeTaxonColor(taxon) }" class="mr-2"></div>
-                <span>{{ fileStore.parsedFile?.taxa.get(taxon).name }}</span>
+                <span>{{ fileStore.parsedFile?.taxa.get(taxon)!.name }}</span>
             </div>
         </div>
     </div>
@@ -13,6 +13,10 @@
         <img src="@/assets/loading_animation.gif" />
         <p>Loading...</p>
     </div>
+
+    <v-btn class="mt-5" color="primary" @click="() => onBack($router)">
+        Back
+    </v-btn>
 </template>
 
 <script setup lang="ts">
@@ -21,6 +25,7 @@ import useFileStore from '@/stores/FileStore';
 import useVisualisationStore from '@/stores/VisualisationStore';
 import {onMounted, ref} from "vue";
 import ColorConstants from "@/logic/constants/ColorConstants";
+import { Router } from 'vue-router';
 
 const fileStore = useFileStore();
 const visualisationStore = useVisualisationStore();
@@ -36,4 +41,8 @@ onMounted(async () => {
 const computeTaxonColor = (taxonId: number) => {
     return ColorConstants.LEGEND[visualisationStore.highlightedTaxa.indexOf(taxonId)];
 }
+
+const onBack = (router: Router) => {
+    router.push("/selection");
+};
 </script>
