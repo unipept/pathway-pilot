19a503b46f5d5cd5853533c20507c8e0eca781b4
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2023-01-19 11:36:54 +0100

    Change lists into sets

--- web/src/logic/communicators/KEGGCommunicator.ts
+++ web/src/logic/communicators/KEGGCommunicator.ts
@@ -1,12 +1,23 @@
 import ParsedFile from "@/logic/parser/ParsedFile";
 
 export default class KEGGCommunicator {
-    // constructor(
-    //     private readonly parsedFile: ParsedFile
-    // ) {}
-    //
-    // getKEGGMap(
-    //     pathwayId: string,
-    //     highlightedTaxa?: number[],
-    // )
+    constructor(
+        private readonly parsedFile: ParsedFile
+    ) {}
+
+    /**
+     * Returns the URL to retrieve an image of the given pathway ID from the KEGG API on which all enzymes that are
+     * associated with the list of highlighted taxa will be highlighted. If no taxa to be highlighted are given, all
+     * nodes will look the same.
+     *
+     * @param pathwayId
+     * @param highlightedTaxa
+     */
+    getKEGGMap(
+        pathwayId: string,
+        highlightedTaxa?: number[]
+    ) {
+
+        const url = `https://www.kegg.jp/kegg-bin/show_pathway?map00400/1.14.16.1%09,blue/1.14.16.1%09,red/multi`
+    }
 }
--- web/src/logic/parser/ParsedFile.ts
+++ web/src/logic/parser/ParsedFile.ts
@@ -7,9 +7,9 @@ export default class ParsedFile {
         public readonly taxa: Map<number, Taxon>,
         public readonly pathways: Map<string, PathwayEntry>,
         public readonly ecs: Map<string, ECEntry>,
-        public readonly taxaToPathways: Map<number, PathwayEntry[]>,
-        public readonly taxaToEcs: Map<number, ECEntry[]>,
-        public readonly pathwaysToEcs: Map<string, ECEntry[]>,
-        public readonly pathwaysToTaxa: Map<string, Taxon[]>
+        public readonly taxaToPathways: Map<number, Set<PathwayEntry>>,
+        public readonly taxaToEcs: Map<number, Set<ECEntry>>,
+        public readonly pathwaysToEcs: Map<string, Set<ECEntry>>,
+        public readonly pathwaysToTaxa: Map<string, Set<Taxon>>
     ) {}
 }
