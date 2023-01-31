8800778443f5a9ddb70b3a85df8146cb76b9b345
tibvdm <tibovandemoortele@hotmail.com>
2023-01-31 14:19:27 +0100

    hide legend when empty

--- web/src/components/legends/TaxonLegend.vue
+++ web/src/components/legends/TaxonLegend.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="legend-container">
+    <div v-if="!empty" class="legend-container">
         <span v-if="title" class="text-h6">{{ title }}</span>
         <div class="d-flex" v-for="item in items">
             <v-icon :color="item.color">mdi-square</v-icon>
@@ -9,6 +9,7 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { LegendItem } from './LegendItem';
 
 export interface Props {
@@ -17,7 +18,9 @@ export interface Props {
     title?: string;
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const empty = computed(() => props.items.length === 0);
 </script>
 
 <style scoped>
