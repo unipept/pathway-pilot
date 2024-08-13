1d9289f99625e8b86cabe0814da8c114aff05d5e
tibvdm <tibovandemoortele@hotmail.com>
2024-08-13 15:47:01 +0200

    set json headers for the new api

--- web/src/logic/communicators/UnipeptCommunicator.ts
+++ web/src/logic/communicators/UnipeptCommunicator.ts
@@ -8,19 +8,19 @@ export default class UnipeptCommunicator {
     ) {}
 
     public async fetchPeptideInfo(peptideList: string[], chunksize=100) {
-        return this.batchedFetch(`${this.baseUrl}/peptinfo.json`, peptideList, chunksize);
+        return this.batchedFetch(`${this.baseUrl}/peptinfo`, peptideList, chunksize);
     }
 
     public async fetchProteinInfo(proteinList: string[], chunksize=100) {
-        return this.batchedFetch(`${this.baseUrl}/protinfo.json`, proteinList, chunksize);
+        return this.batchedFetch(`${this.baseUrl}/protinfo`, proteinList, chunksize);
     }
 
     // TODO: what in case of more than 1000 ids?
     // Fetch id=1 and 99 other ids, then combine trees.
     // Start at the root for both trees, as soon as the trees diverge, add the other tree as a child.
     public async fetchTaxonomy(ids: number[]) {
-        const url = `${this.baseUrl}/taxa2tree.json?input[]=${ids.join("&input[]=")}`;
-        return fetch(url, { method: 'POST' })
+        const url = `${this.baseUrl}/taxa2tree?input[]=${ids.join("&input[]=")}`;
+        return fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' } })
             .then(response => response.json());
     }
 
@@ -31,7 +31,7 @@ export default class UnipeptCommunicator {
             const chunk = items.slice(i, i + chunksize);
 
             const fetchUrl = `${url}?input[]=${chunk.join("&input[]=")}`;
-            await fetch(fetchUrl, { method: 'POST' })
+            await fetch(fetchUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' } })
                 .then(response => response.json())
                 .then(data => result.push(...data))
                 .then(() => this.progressListener.onProgressUpdate(i / items.length));
